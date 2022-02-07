f582ee2d0409956b1218afc0a50be56527543ccc
nacerhadjsaid <nacer.hadjsaid@gmail.com>
2022-02-06 20:01:17 -0500

    comment added

--- Instagram1/src/main/java/us/piit/App.java
+++ Instagram1/src/main/java/us/piit/App.java
@@ -4,8 +4,8 @@ package us.piit;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    //comment added for demo purpose
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
